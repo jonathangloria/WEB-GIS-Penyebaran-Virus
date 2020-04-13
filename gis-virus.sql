8a089809b757de6637b601072570a4e4ba9213df
jonathangloria <jonathanidg@student.ce.undip.ac.id>
2020-04-13 16:32:22 +0700

    Add database file (sql)

--- gis-virus.sql
+++ gis-virus.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 08 Apr 2020 pada 16.37
+-- Generation Time: 12 Apr 2020 pada 19.27
 -- Versi Server: 5.6.43-log
 -- PHP Version: 5.6.28
 
@@ -48,12 +48,13 @@ CREATE TABLE `tbl_penyebaran` (
 --
 
 INSERT INTO `tbl_penyebaran` (`id`, `nama_wilayah`, `provinsi`, `kabupaten`, `kecamatan`, `nama_virus`, `jml_korban`, `jml_meninggal`, `jml_sembuh`, `latitude`, `longitude`, `radius`, `warna`, `foto`) VALUES
-(1, 'Griya M-45', 'Jawa Tengah', 'Kota Semarang', 'Kecamatan Tembalang', 'Virus Hanta', 22, 2, 20, '-6.995607632258654', '110.41602669811331', 50000, 'yellow', ''),
+(1, 'Griya M-45', 'Jawa Tengah', 'Kota Semarang', 'Kecamatan Tembalang', 'Virus Hanta', 22, 3, 19, '-6.995607632258654', '110.41602669811331', 50000, 'yellow', NULL),
 (2, 'Perumahan Sukorejo Indah', 'Jawa Timur', 'Kota Kediri', 'Kecamatan Ngasem', 'Virus Cacar Air', 55, 7, 48, '-7.852498637813016', '112.01913710859974', 30000, 'green', ''),
 (4, 'Kampung Inggris', 'Jawa Timur', 'Kabupaten Kediri', 'Kecamatan Pare', 'Virus Flu Burung', 1200, 14, 1186, '-7.80556171687671', '112.00012323840808', 70000, 'blue', NULL),
 (5, 'Temuwangi', 'Jawa Tengah', 'Klaten', 'Pedan', 'Virus Influenza', 6, 4, 2, '-7.71677532615073', '110.70717577467552', 10000, 'green', 'image-200408-14da92f2bd.jpg'),
-(6, 'Baleharjo', 'Yogyakarta', 'Gunung Kidul', 'Wonosari', 'Virus Influenza', 18, 3, 15, '-7.974917742990998', '110.61396332286505', 10000, 'green', 'image-200408-d0e7b521c1.jpg'),
-(7, 'Dr. Harun II', 'Lampung', 'Bandar Lampung', 'Kota Baru', 'Virus Rabies', 4, 1, 3, '-5.419319145930335', '105.2744422862872', 10000, 'green', 'image-200408-799974951f.jpg');
+(7, 'Dr. Harun II', 'Lampung', 'Bandar Lampung', 'Kota Baru', 'Virus Rabies', 4, 1, 3, '-5.419319145930335', '105.2744422862872', 10000, 'green', 'image-200408-799974951f.jpg'),
+(8, 'Jalan Supriyadi', 'Jawa Tengah', 'Tulungangung', 'Tamanan', 'Virus cacar', 4, 1, 3, '-8.073506518504688', '111.90055845688826', 10000, 'yellow', 'image-200411-9c95b619ad.jpg'),
+(9, 'Kampung Kulon', 'Kalimantan Timur', 'Balikpapan', 'Karangrejo', 'Virus Hanta', 1111, 211, 900, '-1.2366383658699305', '116.83728218633297', 50000, 'yellow', NULL);
 
 -- --------------------------------------------------------
 
@@ -146,8 +147,8 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (1, 2, 'Pemetaan', 'operator', 'fa fa-dashboard', 1),
 (2, 2, 'Data Penyebaran', 'operator/data_penyebaran', 'fa fa-table', 1),
 (3, 1, 'Input Data', 'admin/input', 'fa fa-plus', 1),
-(4, 2, 'Carto (Negara Jerman)', 'operator/carto', 'fa fa-desktop', 1),
-(5, 2, 'Logout', 'auth/logout', 'fas fa-sign-out-alt', 1);
+(4, 2, 'Carto (Negara Jerman)', 'operator/carto', 'fa fa-desktop', 0),
+(5, 2, 'Logout', 'auth/logout', 'fa fa-sign-out', 1);
 
 --
 -- Indexes for dumped tables
@@ -195,7 +196,7 @@ ALTER TABLE `user_sub_menu`
 -- AUTO_INCREMENT for table `tbl_penyebaran`
 --
 ALTER TABLE `tbl_penyebaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
